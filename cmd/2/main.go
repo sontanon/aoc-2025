9c07840d2958be3df1b1bcd818d6ea6228285306
Santiago Ontañón Sánchez <sontanon@gmail.com>
2025-12-02 11:21:19 -0600

    Complete day 2 part 1

--- cmd/2/main.go
+++ cmd/2/main.go
@@ -1,10 +1,158 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+const (
+	spanBufferSize   = 8
+	resultBufferSize = 64
+	numWorkers       = 4
+)
+
+type Span struct {
+	Start int
+	End   int
+}
+
+func ParseSpan(s string) (Span, error) {
+	before, after, found := strings.Cut(s, "-")
+	if !found {
+		return Span{}, fmt.Errorf("invalid span string: %s", s)
+	}
+	start, err := strconv.Atoi(before)
+	if err != nil {
+		return Span{}, fmt.Errorf("invalid start in span string: %s", s)
+	}
+	end, err := strconv.Atoi(after)
+	if err != nil {
+		return Span{}, fmt.Errorf("invalid end in span string: %s", s)
+	}
+	if start <= 0 || end <= 0 || end < start {
+		return Span{}, fmt.Errorf("invalid span range in span string: %s", s)
+	}
+	return Span{Start: start, End: end}, nil
+}
+
+func (r Span) GetInvalidIds() []int {
+	invalids := make([]int, 0)
+	// This should be optimized by reducing the search space and automatically rejecting certain ranges.
+	for i := r.Start; i <= r.End; i++ {
+		if isInvalid(i) {
+			invalids = append(invalids, i)
+		}
+	}
+	return invalids
+}
+
+func isInvalid(id int) bool {
+	// Option 1: strconv.Itoa: 0.01325s per full run
+	// strId := strconv.Itoa(id)
+	// numDigits := len(strId)
+	// if numDigits%2 != 0 {
+	// 	return false
+	// }
+	// return strId[:numDigits/2] == strId[numDigits/2:]
+
+	// Option 2: math.Log10: 0.00662s per full run
+	// numDigits := int(math.Log10(float64(id))) + 1
+	// if numDigits%2 != 0 {
+	// 	return false
+	// }
+	// left := id / int(math.Pow10(numDigits/2))
+	// right := id % int(math.Pow10(numDigits/2))
+	// return left == right
+
+	// Option 3: single loop: 0.00238s per full run
+	left := id
+	right := 0
+	multiplier := 1
+	for {
+		rightDigit := left % 10
+		left /= 10
+		right = rightDigit*multiplier + right
+		multiplier *= 10
+		if left == right {
+			return true
+		}
+		if left < multiplier*10 {
+			return false
+		}
+	}
+}
 
 func Part1(input io.Reader) (int, error) {
-	return 0, nil
+	data, err := io.ReadAll(input)
+	if err != nil {
+		return 0, fmt.Errorf("error reading input: %w", err)
+	}
+
+	spanStrs := strings.Split(strings.TrimSpace(string(data)), ",")
+
+	spans := make(chan Span, spanBufferSize)
+	results := make(chan int, resultBufferSize)
+
+	go func() {
+		for i, spanStr := range spanStrs {
+			span, err := ParseSpan(spanStr)
+			if err != nil {
+				panic(fmt.Errorf("error parsing span on index %d: %w", i, err))
+			}
+			spans <- span
+		}
+		close(spans)
+	}()
+
+	wg := sync.WaitGroup{}
+	for range numWorkers {
+		wg.Go(func() {
+			localSum := 0
+			for span := range spans {
+				invalids := span.GetInvalidIds()
+				for _, id := range invalids {
+					localSum += id
+				}
+			}
+			results <- localSum
+		})
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	totalSum := 0
+	for sum := range results {
+		totalSum += sum
+	}
+
+	return totalSum, nil
+}
+
+func getInputPath() string {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	return filepath.Join(dir, "input.txt")
 }
 
 func main() {
+	file, err := os.Open(getInputPath())
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	invalidIds, err := Part1(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Part 1:", invalidIds)
 }
--- cmd/2/main_test.go
+++ cmd/2/main_test.go
@@ -1,9 +1,79 @@
 package main
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
+
+func TestGetInvalidIds(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected []int
+	}{
+		{
+			"11-22",
+			[]int{11, 22},
+		},
+		{
+			"95-115",
+			[]int{99},
+		},
+		{
+			"998-1012",
+			[]int{1010},
+		},
+		{
+			"1188511880-1188511890",
+			[]int{1188511885},
+		},
+		{
+			"222220-222224",
+			[]int{222222},
+		},
+		{
+			"1698522-1698528",
+			[]int{},
+		},
+		{
+			"446443-446449",
+			[]int{446446},
+		},
+		{
+			"38593856-38593862",
+			[]int{38593859},
+		},
+		{
+			"565653-565659",
+			[]int{},
+		},
+		{
+			"824824821-824824827",
+			[]int{},
+		},
+		{
+			"2121212118-2121212124",
+			[]int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			span, err := ParseSpan(tt.name)
+			if err != nil {
+				t.Fatalf("ParseSpan error = %v", err)
+			}
+			result := span.GetInvalidIds()
+			if len(result) != len(tt.expected) {
+				t.Fatalf("GetInvalidIds unexpected length, got = %v, want %v", result, tt.expected)
+			}
+			for i := range result {
+				if result[i] != tt.expected[i] {
+					t.Errorf("GetInvalidIds[%d] got = %v, want %v", i, result[i], tt.expected[i])
+				}
+			}
+		})
+	}
+}
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
@@ -25,6 +95,42 @@ func TestPart1(t *testing.T) {
 			}
 			if result != tt.expected {
 				t.Errorf("Part 1 got = %v, want %v", result, tt.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	data, err := os.ReadFile(getInputPath())
+	if err != nil {
+		b.Fatalf("failed to read input file: %v", err)
+	}
+	input := string(data)
+
+	tests := []struct {
+		name     string
+		fn       func(string) (int, error)
+		expected int
+	}{
+		{
+			name: "Part 1",
+			fn: func(input string) (int, error) {
+				return Part1(strings.NewReader(input))
+			},
+			expected: 44487518055,
+		},
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			b.ReportAllocs()
+			for b.Loop() {
+				result, err := tt.fn(input)
+				if err != nil {
+					b.Fatalf("benchmark failed: %v", err)
+				}
+				if result != tt.expected {
+					b.Fatalf("expected %d, got %d", tt.expected, result)
+				}
 			}
 		})
 	}
